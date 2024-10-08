9a97460b56c396bf75ed6a76a216fd4475db6b11
Grecia2502 <grecia.glz0225.1@gmail.com>
2024-10-08 13:33:46 -0600

    mi primer projecto

---
+++ mavenprojectjah/src/main/java/com/mycompany/mavenprojectjah/Mavenprojectjah.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenprojectjah;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Mavenprojectjah {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
